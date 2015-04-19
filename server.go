804cf4acfcf1ec60c690fecc6fe4e98a01130274
Brandon L. Gauthier <Brandon.L.Gauthier@gmail.com>
2015-04-18 23:25:45 -0400

    added custom endpoint

--- server.go
+++ server.go
@@ -1,24 +1,28 @@
 package main
 
-import (  
-    // Standard library packages
-    "fmt"
-    "net/http"
+import (
+	// Standard library packages
+	"fmt"
+	"net/http"
 
-    // Third party packages
-    "github.com/julienschmidt/httprouter"
+	// Third party packages
+	"github.com/julienschmidt/httprouter"
 )
 
-func main() {  
-    // Instantiate a new router
-    r := httprouter.New()
+func main() {
+	// Instantiate a new router
+	r := httprouter.New()
 
-    // Add a handler on /test
-    r.GET("/test", func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-        // Simply write some test data for now
-        fmt.Fprint(w, "Welcome!\n")
-    })
+	// Add a handler on /test
+	r.GET("/test", func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+		// Simply write some test data for now
+		fmt.Fprint(w, "Welcome!\n")
+	})
 
-    // Fire up the server
-    http.ListenAndServe("localhost:3000", r)
+	r.GET("/blg", func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+		fmt.Fprint(w, "blg Test!\n")
+	})
+
+	// Fire up the server
+	http.ListenAndServe("localhost:3000", r)
 }
