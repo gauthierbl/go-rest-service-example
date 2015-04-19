cbe013fbb154b788eb37077a1934cb56bce1f901
Brandon L. Gauthier <Brandon.L.Gauthier@gmail.com>
2015-04-19 00:14:23 -0400

    added model example

--- server.go
+++ server.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	// Standard library packages
+	"encoding/json"
 	"fmt"
 	"net/http"
 
 	// Third party packages
+	"github.com/gauthierbl/go-rest-service-example/models"
 	"github.com/julienschmidt/httprouter"
 )
 
@@ -13,10 +15,23 @@ func main() {
 	// Instantiate a new router
 	r := httprouter.New()
 
-	// Add a handler on /test
-	r.GET("/test", func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-		// Simply write some test data for now
-		fmt.Fprint(w, "Welcome!\n")
+	// GET a user
+	r.GET("/user/:id", func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+
+		user := models.User{
+			Name:   "Brandon G",
+			Gender: "male",
+			Age:    30,
+			ID:     p.ByName("id"),
+		}
+
+		// Marshal to JSON
+		userAsJSON, _ := json.Marshal(user)
+
+		//Write content-type, statuscode and payload
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(200)
+		fmt.Fprint(w, string(userAsJSON))
 	})
 
 	r.GET("/blg", func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
